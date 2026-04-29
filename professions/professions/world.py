bcdc09a8f578c120ce5ad353e9b1f777799c3db1
dhdh75evn96vjefh-commits <dhdh75evn96vjefh@gmail.com>
2026-04-29 15:32:42 +0300

    Add WorldManager class for virtual world management

---
+++ professions/professions/world.py
@@ -0,0 +1,5 @@
+class WorldManager:
+    def __init__(self):
+        self.map_name = "فلسطين - العالم الافتراضي"
+    def get_location(self, player):
+        return f"اللاعب في وسط المدينة حالياً."
