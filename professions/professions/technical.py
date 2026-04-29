4f8ed2fa887ee2246b883154638038f158141481
dhdh75evn96vjefh-commits <dhdh75evn96vjefh@gmail.com>
2026-04-29 15:31:36 +0300

    Add Engineer and Mechanic classes in technical.py

---
+++ professions/professions/technical.py
@@ -0,0 +1,12 @@
+class Engineer:
+    def __init__(self):
+        self.role = "مهندس"
+    def work(self):
+        return "المهندس يشرف على بناء وتطوير العقارات."
+
+class Mechanic:
+    def __init__(self):
+        self.role = "ميكانيكي"
+    def work(self):
+        return "الميكانيكي يقوم بصيانة مركبات اللاعبين."
+
